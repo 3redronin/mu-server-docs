619b421108cb287daef7886e8ccaee25317270c3
Daniel Flower <git@danielflower.com>
2019-02-17 22:30:13 +0800

    Initial hello-world with working deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,17 @@ set -x
 if [[ -f 'pom.xml' ]];
 then
 
-  scp deploy.sh ubuntu@18.236.152.164:~
-  mvn -B releaser:release
-  scp target/docs-*.jar ubuntu@18.236.152.164:~
-  ssh ubuntu@18.236.152.164 'deploy.sh'
+  scp deploy.sh ubuntu@muserver.io:~
+#  mvn -B releaser:release
+  mvn clean package
+  scp target/docs-*.jar ubuntu@muserver.io:~
+  ssh ubuntu@muserver.io 'bash deploy.sh'
 
 else
 
   ls -la
+  pkill -9 -f 'docs' || echo "Nothing killed"
+
+  nohup java -jar docs*.jar &
 
 fi
