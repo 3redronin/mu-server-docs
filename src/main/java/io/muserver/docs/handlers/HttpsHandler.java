6eb26d800c368bfb395de7c09b9e5dd035b560c4
Daniel Flower <git@danielflower.com>
2023-01-23 14:49:34 +0800

    Auto detect tcnative version

--- src/main/java/io/muserver/docs/handlers/HttpsHandler.java
+++ src/main/java/io/muserver/docs/handlers/HttpsHandler.java
@@ -1,12 +1,11 @@
 package io.muserver.docs.handlers;
 
-import io.muserver.MuRequest;
-import io.muserver.MuResponse;
-import io.muserver.RouteHandler;
-import io.muserver.SSLInfo;
+import io.muserver.*;
 import io.muserver.docs.ViewRenderer;
 
+import java.io.InputStream;
 import java.util.Map;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class HttpsHandler implements RouteHandler {
@@ -27,7 +26,30 @@ public class HttpsHandler implements RouteHandler {
                 .with("sslProtocols", String.join(", ", sslInfo.protocols()))
                 .with("sslCiphers", String.join(", ", sslInfo.ciphers()))
                 .with("certs", sslInfo.certificates().stream().map(cert -> cert.getSubjectX500Principal() + " - from " + cert.getNotBefore() + " to " + cert.getNotAfter()).collect(Collectors.toList()))
+                .with("tcnativeVersion", version("io.netty", "netty-tcnative-boringssl-static"))
         );
+    }
+
+
+    static String version(String groupId, String artifactId) {
+        String v;
+        try {
+            Properties props = new Properties();
+            InputStream in = MuServer.class.getResourceAsStream("/META-INF/maven/" + groupId + "/" + artifactId + "/pom.properties");
+            if (in == null) {
+                v = "RELEASE";
+            } else {
+                try {
+                    props.load(in);
+                } finally {
+                    in.close();
+                }
+                v = props.getProperty("version");
+            }
+        } catch (Exception ex) {
+            v = "RELEASE";
+        }
+        return v;
     }
 
 }
